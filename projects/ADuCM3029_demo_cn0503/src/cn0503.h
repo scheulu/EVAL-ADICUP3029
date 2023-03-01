891e44999b96505bbdf9c798fca3f4dfa26c9ee3
angeheli1 <102286485+angeheli1@users.noreply.github.com>
2023-03-01 15:26:30 +0100

    Update cn0503.h
    Test

--- projects/ADuCM3029_demo_cn0503/src/cn0503.h
+++ projects/ADuCM3029_demo_cn0503/src/cn0503.h
@@ -4,7 +4,7 @@
 ********************************************************************************
  * Copyright 2019(c) Analog Devices, Inc.
  *
- * All rights reserved.
+ * All rights reserved. by Büffelcrew
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
